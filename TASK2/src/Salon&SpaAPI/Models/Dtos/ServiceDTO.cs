3fe4923c2112d7099e3ebdee800d5d2821e7c04a
Marie_alvrz <marie852006@gmail.com>
2026-02-10 19:39:23 -0400

    Uploading task2

---
+++ TASK2/src/Salon&SpaAPI/Models/Dtos/ServiceDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Salon_SpaAPI.Models.Dtos
+{
+    public class ServiceDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Price { get; set; }
+    }
+}
---
+++ TASK2/src/Salon&SpaAPI/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Salon_SpaAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<Salon_SpaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MainConnection")));
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
