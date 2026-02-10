d7e2e565f56d6ee19334ff4dcd09b7e5f40e3ac5
Marie_alvrz <marie852006@gmail.com>
2026-02-10 19:30:24 -0400

    Uploading Task2

---
+++ TASK2/src/Salon&SpaAPI/Models/Entities/Appointment.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Salon_SpaAPI.Models.Entities
+{
+    public class Appointment
+    {
+  
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public DateTime AppointmentDate { get; set; }
+        public int ServiceId { get; set; }
+        public Service? Service { get; set; }
+
+    }
+}
